30b54fbffaec61d05f72b1e683b2fb3281d4b8de
Saiake <sergey.odinokih@yandex.ru>
2023-02-21 19:09:27 +0500

    worker_threads add

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
-const ExcelReader = require ('./ExcelReader.js')
 const { Telegraf } = require('telegraf')
+const { Worker } = require('node:worker_threads')
 
-const filename = './sales.xlsx'
-ExcelReader.getPrices(filename)
-ExcelReader.getData(filename)
+function runService(data) {
+  return new Promise((resolve, reject) => {
+    const worker = new Worker('./ExcelReader.js', { workerData: data });
+    worker.on('message', resolve);
+    worker.on('error', reject);
+    worker.on('exit', (code) => {
+      if (code !== 0)
+        reject(new Error(`Worker stopped with exit code ${code}`));
+    })
+  })
+}
 
 const token = '6034191716:AAEMdbm9eJGn4-0C9r0UzLcZOw7JWqEC4Vw'
-const bot = new Telegraf(token);
-console.log('ok')
-bot.start((ctx) => {
-    let obj = new ExcelReader
-    obj.canBuy(ExcelReader.mainArray, [0,0,0], [30,30,30])
-    ctx.reply('Send me ')
+const bot = new Telegraf(token, {handlerTimeout: 9_000_000});
+
+bot.start(async (ctx) => {
+    console.log('ok')
+    runService(
+      {
+        params: [0,0,0], 
+        wanted: [30,30,30]
+      }).then(msg => {
+        ctx.reply('Send me ')
+      })
 });
+
 bot.help((ctx) => ctx.reply('Send me a sticker'));
 bot.on('sticker', (ctx) => ctx.reply('👍'));
 bot.hears('hi', (ctx) => ctx.reply('Hey there'));
